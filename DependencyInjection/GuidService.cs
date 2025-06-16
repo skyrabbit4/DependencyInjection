54dde59d14d25b5c34f164872d371d1bb4025a46
skyrabbit4 <111689974+skyrabbit4@users.noreply.github.com>
2025-06-16 23:32:27 +0530

    added guideservice

---
+++ DependencyInjection/GuidService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DependencyInjection
+{
+    /// <summary>
+  /// Used to demonstrate DI lifetimes.
+  /// Each instance gets a new GUID when constructed.
+  /// </summary>
+    public class GuidService
+    {
+        // Readonly property set once at construction
+        public Guid Id { get; } = Guid.NewGuid();
+    }
+}
+
